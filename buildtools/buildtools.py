fc18128e09da0466030c0ac592cdc175802c290d
Thomas Pingel <thomas.pingel@gmail.com>
2023-06-28 16:22:32 -0400

    better scoring

--- buildtools/buildtools.py
+++ buildtools/buildtools.py
@@ -503,3 +503,49 @@ def battersby_normalize(a,b):
     else:
         return -(b/a - 1)
     
+    
+#%%
+
+
+def build_score(xy,ab,t):
+    
+    results = {}
+    
+    # Chamfer
+    chamfer_xy_to_ab = chamfer_distance(xy,ab,direction='x_to_y')
+    chamfer_ab_to_xy = chamfer_distance(xy,ab,direction='y_to_x')
+    if chamfer_xy_to_ab >= chamfer_ab_to_xy:
+        chamfer_ratio = chamfer_xy_to_ab / chamfer_ab_to_xy - 1
+    else:
+        chamfer_ratio = -(chamfer_ab_to_xy / chamfer_xy_to_ab - 1)
+    results['chamfer_xy_to_ab'] = chamfer_xy_to_ab
+    results['chamfer_ab_to_xy'] = chamfer_ab_to_xy
+    results['chamfer_ratio']    = chamfer_ratio    
+
+    if len(xy) <= len(ab):  # At least as many object detected as exist
+        row,col,costs = hungarian_algorithm(xy,ab)
+        ab_paired = ab[col]
+        bdr_result = bt.bdr(xy,ab_paired)
+        d = np.linalg.norm(xy - ab_paired, axis=1)         # Distance
+    else:                   # Fewer objects detected than exist
+        row,col,costs = hungarian_algorithm(ab,xy)
+        xy_paired = xy[col]
+        bdr_result = bt.bdr(xy_paired,ab)
+        d = np.linalg.norm(xy_paired - ab, axis=1) 
+        
+    results['bdr'] = bdr_result
+    results['distance'] = d
+    results['col'] = col            # This is the Hungarian Distance lookup
+
+    TP = np.sum(d<t)
+    FN = len(xy) - TP
+    FP = len(ab) - TP
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN) 
+    F1 = 2 * (precision * recall) / (precision + recall)
+    results['n'] = TP + FN + FP
+    results['TP']=TP;results['FN']=FN;results['FP']=FP;results['precision']=precision
+    results['recall']=recall;results['F1']=F1;
+    
+    return results
+    
